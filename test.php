d86831a0f4b30f414dc181ab16eb86d689832b87
Cory Collier <cory.collier@ahss.org>
2017-04-18 15:38:48 -0400

    trimming the test script

--- test.php
+++ test.php
@@ -13,27 +13,6 @@ $results = $builder
     ->process($urls)
     ->getResults();
 
-file_put_contents('tmp/processed', serialize($results));
-
 print_r($results->getMetrics('technology-stack'));
 print_r($results->getMetrics('css-complexity'));
 print_r($results->getMetrics('dns-data'));
-
-die;
-$ips = [];
-$cssScores = [];
-foreach ($results as $result) {
-    $uri = $result->getUri();
-    $metrics = $result->getMetrics();
-    $dnsData = $metrics['dns-data']->getData();
-    $ip = $dnsData[0]['address'];
-    if (! array_key_exists($ip, $ips)) {
-        $ips[$ip] = [];
-    }
-    $ips[$ip][] = $uri;
-    $cssScores[$uri] = $metrics['css-complexity']->getScore();
-}
-
-print_r($results);
-print_r($ips);
-print_r($cssScores);
